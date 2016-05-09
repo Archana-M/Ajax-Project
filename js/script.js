ca6352bb2ce93697c87b2940bef91d77ba53b099
ArchanaMudiam <archanamudiam@gmail.com>
2016-05-09 16:41:52 -0700

    second commit

--- js/script.js
+++ js/script.js
@@ -11,9 +11,8 @@ function loadData(event) {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
+    // - *********** - Google  streetview - *********** -
 
-    // YOUR CODE GOES HERE!
 
     var streetStr = $('#street').val();
     var cityStr = $("#city").val();
@@ -25,6 +24,29 @@ function loadData(event) {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
 
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+    //   - *********** - End of Google Street View API - *********** -
+
+    // - *********** - NY Times API - *********** -
+
+    var newsUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+    cityStr + '&sort=newest&api-key=4860314b78b546f9b58b28ef25f21da1'
+
+    $.getJSON(newsUrl, function ( data ){
+
+        $nytHeaderElem.text('New York Times Ariticle About' + cityStr);
+
+        articles = data.response.docs;
+        for(var i = 0; i<articles.length; i++){
+            var article = articles[i];
+            $nytElem.append('<li class = "article">' + '<a href ="'+article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+        };
+
+
+    });
+
+
+    // - *********** - End of NY Times API - *********** -
 
 
 
